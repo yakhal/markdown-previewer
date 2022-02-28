76c9eeb4883fed7979ab4cfd1e222d1ccaae18a7
yakhal <rastogi.p39@gmail.com>
2022-02-28 20:04:03 +0530

    Implemented markdown functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+import MarkdownView from 'react-showdown';
 
 function App() {
+  const [inputText, setInputText] = useState(`### Don't Know how to write MD? 
+[Get a quick tutorial](https://markdown-it.github.io/)`);
+
+  const onChangeHandler = (event) => {
+    setInputText(event.target.value);
+  }
+
   return (
     <div className="app">
       <header>
@@ -9,11 +17,15 @@ function App() {
       <div className="container">
         <div className="md-writer">
           <h2>Type Here...</h2>
-          <textarea></textarea>
+          <textarea value={inputText} onChange={onChangeHandler}></textarea>
         </div>
         <div className="previewer">
           <h2>MD Preview</h2>
           <div className='result'>
+            <MarkdownView
+              markdown={inputText}
+              options={{ tables: true, emoji: true }}
+            />
           </div>
         </div>
       </div>
